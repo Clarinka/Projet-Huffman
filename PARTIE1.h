6bf6ef91676a36c44936be1ef43c8dca2d9d5fa6
mahrazcam <73132394+mahrazcam@users.noreply.github.com>
2020-12-02 20:30:30 +0100

    Create PARTIE1.h
    Prototypes des fonctions de partie1.c avec documentation

---
+++ PARTIE1.h
@@ -0,0 +1,32 @@
+#ifndef PARTIE1_h
+#define PARTIE1_h
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* \brief Function to convert a character to bits.
+* \return nothing it is a void function.
+*/
+
+
+void convertir_binaire(int*, char*);
+
+/**
+ *\brief Function to convert characters via a text file to bits.
+* \return nothing it is a void function.
+ */
+void equivalent_fichier_texte();
+
+/**
+* \brief Function to count the number of characters in a text file
+* \return number of characters in a text file
+*/
+int compter_carac_txt();
+
+/**
+* \brief Function to count the number of characters in a binary text file
+* \return number of characters in a binary text file
+*/
+int compter_carac_txt_bin();
+
+#endif // PARTIE1_h
