5d4bb9046f53cb520753f02c5948df0c0bdfde2d
Clarinka <73124491+Clarinka@users.noreply.github.com>
2020-12-02 20:31:03 +0100

    Update ProjetHuffman2bis.c
    AJOUT DU MAIN

--- ProjetHuffman2bis.c
+++ ProjetHuffman2bis.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// ProjetHuffman2bis.c : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
-
+#include "PARTIE2.h"
+#include "PARTIE1.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    FILE* fichier = NULL;
+    FILE* fichier_binaire = NULL;
+    Element* Liste_caractere;
+
+    equivalent_fichier_texte();
+    printf("nb de carac Alice.txt : %d \n",compter_carac_txt(fichier));
+    printf("nb de carac AliceBinaire.txt : %d \n",compter_carac_txt_bin(fichier_binaire));
+
+    Liste_caractere = occurrence_caractere();
+    afficher_Liste(Liste_caractere);
+
+    fclose(fichier);
+    fclose(fichier_binaire);
+    liberer_liste(&Liste_caractere);
+
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
