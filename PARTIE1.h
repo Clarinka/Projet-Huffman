e2bee7f090766188f984211a9ef41c6db4cfd474
mahrazcam <73132394+mahrazcam@users.noreply.github.com>
2020-12-02 17:39:08 +0100

    Create PARTIE1.h
    Création des prototypes des fonctions du fichier PARTIE1.c

---
+++ PARTIE1.h
@@ -0,0 +1,11 @@
+#ifndef PARTIE1_h
+#define PARTIE1_h
+#include <stdio.h>
+#include <stdlib.h>
+
+void convertir_binaire(int* ,char* );
+void equivalent_fichier_texte();
+int compter_carac_txt();
+int compter_carac_txt_bin();
+
+#endif // PARTIE1_h
