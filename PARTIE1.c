6a5908151eba2d3694dda328106ed3782d7bae4f
Clarinka <73124491+Clarinka@users.noreply.github.com>
2020-12-07 18:03:01 +0100

    Update PARTIE1.c

--- PARTIE1.c
+++ PARTIE1.c
@@ -7,20 +7,20 @@ void convertir_binaire(int* tableau, char* caract_a_convertir)
 {
     int i;
 
-    //tableau qui permettra de stocker la valeur du caractère en binaire
+    
     int* tableau2;
     tableau2 = (int*)malloc(100 * sizeof(int));
 
-    //on prend la valeur du caractère et on le met dans un entier
+    
     int nb = *caract_a_convertir;
 
-    //convertie en binaire le nombre
+    
     for(i=0; nb > 0; i++){
         *(tableau2+i) = nb % 2;
         nb = nb / 2;
     }
 
-    //le nombre binaire étant été fait à l'envers on inverse le contenu et on le met dans le bon tableau
+    
     for(i=0;i<8;i++){
         *(tableau+i) = *(tableau2+7-i);
     }
@@ -35,28 +35,17 @@ void equivalent_fichier_texte()
     int i;
     char caractere;
     int caractere2;
-    //char lettre;
+    
 
     FILE* fichier = NULL;
     FILE* fichier_binaire = NULL;
 
     tab = (int*)malloc(100 * sizeof(int));
-    /*
-    printf("Quelle est le caractere? \n");
-    scanf("%c",&lettre);
+   
 
-    convertir_binaire(tab,&lettre);
-
-    for(i=0;i<8;i++){
-        printf("%d ",*(tab+i));
-    }
-
-    */
-
-    // on lit uniquement le fichier Alice
+    
     fichier = fopen("Alice.txt", "r");
 
-    //on écrit dans le fichier AliceBinaire
     fichier_binaire = fopen("AliceBinaire.txt", "r+");
 
     if(fichier == NULL)
@@ -70,11 +59,7 @@ void equivalent_fichier_texte()
             fseek(fichier,0,SEEK_CUR);
             caractere = fgetc(fichier);
             convertir_binaire(tab,&caractere);
-          /*  for(i=0;i<8;i++)
-            {
-                printf("%d ",*(tab+i));
-            }
-            printf("\n");*/
+          
 
             if(caractere!=EOF)
             {
@@ -82,9 +67,9 @@ void equivalent_fichier_texte()
                 {
                     caractere2 = *(tab+i);
                     fprintf(fichier_binaire,"%d",caractere2);
-                    //printf("%d ", caractere2);
+             
                 }
-               // printf("\n");
+              
             }
         } while (caractere!=EOF);
 
