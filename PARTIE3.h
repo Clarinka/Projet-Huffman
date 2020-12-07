921d651586a9072e951239dfccca620220401bc5
mahrazcam <73132394+mahrazcam@users.noreply.github.com>
2020-12-07 18:58:25 +0100

    Update PARTIE3.h

--- PARTIE3.h
+++ PARTIE3.h
@@ -1,1 +1,50 @@
+#ifndef PARTIE3_h
+#define PARTIE3_h
+#include<stdio.h>
+#include<stdlib.h>
 
+
+
+/**
+* \brief Function which, by dichotomous search, adds to an array of nodes an occurrence when the character has already been found, or which adds the node of the character otherwise.
+* \return nothing it is a void function.
+*/
+void ajout_occurrence(int* ,Noeud* );
+
+
+/**
+* \brief Function that sorts an array of nodes based on occurrences.
+* \return nothing it is a void function.
+*/
+void trier_tab_noeud(int*);
+
+/**
+* \brief Function which, using two queues, creates the Huffman tree from an array of nodes sorted by occurrences.
+* \return nothing it is a void function.
+*/
+void creer_Huffman_pile(Queue*, Queue*, int*);
+
+/**
+* \brief Function that organizes the nodes in an AVL according to the order of the characters present.
+* \return the first node of the tree.
+*/
+Noeud* org_noeuds_AVL(Noeud*);
+
+
+/**
+* \brief Function which compresses a text file in an optimized way.
+* \return nothing it is a void function.
+*/
+void compression_texte();
+
+
+
+/**
+* \brief Function which decompresses a text file from a Huffman dictionary file.
+* \return nothing it is a void function.
+*/
+void decompression_texte();
+
+
+
+#endif 
