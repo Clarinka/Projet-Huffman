96959f69be883acb4069b72d4dadbe6ecc3dc3c1
Clarinka <73124491+Clarinka@users.noreply.github.com>
2020-12-07 17:31:43 +0100

    Add files via upload

---
+++ texte.h
@@ -0,0 +1,13 @@
+#ifndef texte_h
+#define texte_h
+#include <stdio.h>
+
+
+void convertir_binaire(int* ,char* );
+void equivalent_fichier_texte();
+int compter_carac_txt();
+int compter_carac_txt_bin();
+
+
+
+#endif /* texte_h */
