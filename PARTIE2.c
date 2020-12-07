9720de085e93424e5951c46fabbdd5e300abe4fa
Clarinka <73124491+Clarinka@users.noreply.github.com>
2020-12-07 18:00:54 +0100

    Update PARTIE2.c

--- PARTIE2.c
+++ PARTIE2.c
@@ -11,10 +11,34 @@ Element* occurrence_caractere()
 }
 
 
-
-
-
 Noeud* création_liste_de_noeud(Element* l)
 {
     return NULL;
+}
+
+
+
+void fichier_texte_arbre_Huffman(Noeud* )
+{
+   return NULL;
+}
+
+
+void traduction_texte_en_suite_binaire()
+{
+   return NULL;
+}
+
+
+
+void compression_fichier_texte()
+{
+   return NULL;
+}
+
+
+
+void decompression_fichier_texte()
+{
+   return NULL;
 }
