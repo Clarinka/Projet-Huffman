d7359120c9db87092423b41c035eb3db1b624c37
Clarinka <73124491+Clarinka@users.noreply.github.com>
2020-12-09 22:48:57 +0100

    Update and rename ProjetHuffman2bis.c to main.c

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "PARTIE2.h"
-#include "PARTIE1.h"
+#include "partie2.h"
+#include "texte.h"
 
 int main()
 {
+    int min, position;
     FILE* fichier = NULL;
     FILE* fichier_binaire = NULL;
-    Element* Liste_caractere;
+
+
+
+    Noeud* Arbre = NULL;
+    Element* Liste_caractere = NULL;
+
 
     equivalent_fichier_texte();
     printf("nb de carac Alice.txt : %d \n",compter_carac_txt(fichier));
     printf("nb de carac AliceBinaire.txt : %d \n",compter_carac_txt_bin(fichier_binaire));
 
+    printf("\n\n");
     Liste_caractere = occurrence_caractere();
     afficher_Liste(Liste_caractere);
+
+
+    Arbre = creation_liste_de_noeud(Liste_caractere,0);
+
+    printf("\n\n");
+    inverser_liste(&Arbre);
+    afficher_Liste_de_noeuds(Arbre);
+    fichier_texte_arbre_Huffman(Arbre);
+
+    trad_text_bin_huffman();
 
     fclose(fichier);
     fclose(fichier_binaire);
