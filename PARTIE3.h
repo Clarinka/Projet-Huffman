017e3034508bdc5db04707ff8139d44c1fb0bbac
Clarinka <73124491+Clarinka@users.noreply.github.com>
2020-12-11 13:28:37 +0100

    Update PARTIE3.h

--- PARTIE3.h
+++ PARTIE3.h
@@ -8,20 +8,20 @@
 * \brief Function which, by dichotomous search, adds to an array of nodes an occurrence when the character has already been found, or which adds the node of the character otherwise.
 * \return nothing it is a void function.
 */
-void ajout_occurrence(int* ,Noeud* );
+void ajout_occurrence();
 
 
 /**
 * \brief Function that sorts an array of nodes based on occurrences.
 * \return nothing it is a void function.
 */
-void trier_tab_noeud(int*);
+void trier_tab_noeud(Noeud );
 
 /**
 * \brief Function which, using two queues, creates the Huffman tree from an array of nodes sorted by occurrences.
 * \return nothing it is a void function.
 */
-void creer_Huffman_pile(Queue*, Queue*, int*);
+void creer_Huffman_pile(Queue* , Queue* , Noeud );
 
 /**
 * \brief Function that organizes the nodes in an AVL according to the order of the characters present.
